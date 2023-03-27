3d3ac2d3cbd9fee6bfa4394fc525634b0e61b867
hamza niazi <hamzaniazi@we-over-i.com>
2023-03-27 17:03:07 +0500

    added the complete focus experience flow

--- tests/Pages/focusSoundExperience.ts
+++ tests/Pages/focusSoundExperience.ts
@@ -47,6 +47,13 @@ async clickFocusTile() {
       const button = await this.page.locator(focusSoundExperienceLocators.leftArrowSoundExperience);
       await button.click();
     }
+//click the beach tile 
+async clickBeachTileSoundExperience(){
+    const button =await this.page.locator(focusSoundExperienceLocators.BeachTileFocusSoundEXperience);
+    await button.click;
+}
+
+    
   async clickTenminSoundExperienceOption(){
       const button = await this.page.locator(focusSoundExperienceLocators.tenMinSoundExperienceOption);
       await button.click();
@@ -60,6 +67,7 @@ async clickFocusTile() {
       await expect(actualText).toContain(expectedPartialText);
   }
 
+
   async clickLearnWhySoundExperience(){
       const button =await this.page.locator(focusSoundExperienceLocators.learnWhySoundExperience);
       await button.click();
@@ -69,12 +77,12 @@ async clickFocusTile() {
       await expect(actualText).toContain(expectedPartialText);
 
   }
-  async clickcloseBinauralBeatsSoundExperiencePopupText(){
+  async clickCloseBinauralBeatsSoundExperiencePopupText(){
       const button =await this.page.locator(focusSoundExperienceLocators.closeBinauralBeatsSoundExperiencePopupText);
       await button.click();
 
   }
-  async clickletsBeginFocusSoundExperienceButton(){
+  async clickLetsBeginFocusSoundExperienceButton(){
       const button =await this.page.locator(focusSoundExperienceLocators.letsBeginFocusSoundExperienceButton);
       await button.click();
     //   const expectedPartialText = 'Full';
@@ -83,13 +91,21 @@ async clickFocusTile() {
     //   await expect(actualText).toContain(expectedPartialText);
  
     }
-    async clickvolumeFocusSoundExperience(){
+    async clickStartExperienceButtonSafariFocusSoundExperience(){
+        const buttonExists = await this.page.isVisible(focusSoundExperienceLocators.startExperienceButtonSafariFocusSoundExperience);
+
+  // If the button exists, click it
+  if (buttonExists) {
+    await this.page.click(focusSoundExperienceLocators.startExperienceButtonSafariFocusSoundExperience);
+  }
+    }
+    async clickVolumeFocusSoundExperience(){
         const button = await this.page.locator(focusSoundExperienceLocators.volumeFocusSoundExperience);
         await button.click();
 
     }
 
-    async binauralFocusSlider(){
+    async clickBinauralFocusSlider(){
         const slider=await this.page.locator(focusSoundExperienceLocators.binauralSliderFocusSoundExperience);
 //         const box = await slider.boundingBox();
 
@@ -125,5 +141,116 @@ const sliderBoundingBox = await slider.boundingBox();
   await this.page.mouse.up();
     }
 
+    async clickMusicSliderFocusSoundExperience(){
+        const slider = await this.page.locator(focusSoundExperienceLocators.musicSliderFocusSoundExperience);
+        const sliderBoundingBox = await slider.boundingBox();
 
-}
+  // calculate the x-coordinate for 50% of the slider
+  const centerX = sliderBoundingBox.x + sliderBoundingBox.width / 2;
+
+  // move the slider to the left by 50 pixels
+  await this.page.mouse.move(centerX, sliderBoundingBox.y);
+  await this.page.mouse.down();
+  await this.page.mouse.move(centerX - 50, sliderBoundingBox.y, { steps: 20 });
+  await this.page.mouse.up();
+
+  // move the slider to the right by 50 pixels
+  await this.page.mouse.move(centerX, sliderBoundingBox.y);
+  await this.page.mouse.down();
+  await this.page.mouse.move(centerX + 50, sliderBoundingBox.y, { steps: 20 });
+  await this.page.mouse.up();
+
+    }
+
+
+    async clickNaturescapeSliderFocusSoundExperience(){
+        const slider=await this.page.locator(focusSoundExperienceLocators.naturescapeSliderFocusSoundExperience);
+        // calculate the x-coordinate for 50% of the slider
+
+        const sliderBoundingBox = await slider.boundingBox();
+  const centerX = sliderBoundingBox.x + sliderBoundingBox.width / 2;
+
+  // move the slider to the left by 50 pixels
+  await this.page.mouse.move(centerX, sliderBoundingBox.y);
+  await this.page.mouse.down();
+  await this.page.mouse.move(centerX - 50, sliderBoundingBox.y, { steps: 20 });
+  await this.page.mouse.up();
+
+  // move the slider to the right by 50 pixels
+  await this.page.mouse.move(centerX, sliderBoundingBox.y);
+  await this.page.mouse.down();
+  await this.page.mouse.move(centerX + 50, sliderBoundingBox.y, { steps: 20 });
+  await this.page.mouse.up();
+    }
+    async clickCloseVolumePopUpFocusSoundEXperience(){
+        const button =await this.page.locator(focusSoundExperienceLocators.closeVolumePopUpFocusSoundEXperience);
+      await button.click();
+    }
+
+    async clickplayPauseFocusSoundExperienceButton(){
+        const button =await this.page.locator(focusSoundExperienceLocators.playPauseFocusSoundExperienceButton);
+      await button.click();
+      const expectedPartialText = 'Paused';
+      const element = await this.page.locator(focusSoundExperienceLocators.pauseTextOnTapToResumePopUpFocusSoundExperience);
+      const actualText = await element.textContent();
+      await expect(actualText).toContain(expectedPartialText);
+    }
+    async clickResumeTextOnTapToResumePopUpFocusSoundExperience(){
+        const button =await this.page.locator(focusSoundExperienceLocators.resumeTextOnTapToResumePopUpFocusSoundExperience);
+        await button.click();
+
+    }
+    async clickFullScreenFocusSoundExperience(){
+        const button =await this.page.locator(focusSoundExperienceLocators.fullScreenFocusSoundExperience);
+        await button.click();
+    }
+    async clickEndExperienceFocusSoundExperienceButton(){
+       const button =await this.page.locator(focusSoundExperienceLocators.endExperienceFocusSoundExperienceButton);
+       await button.click();
+       const expectedPartialText = 'End';
+      const element = await this.page.locator(focusSoundExperienceLocators.endExperincePoupFocusSoundExperienceButton);
+      const actualText = await element.textContent();
+      await expect(actualText).toContain(expectedPartialText);
+
+
+    }
+    async clickEndExperincePoupFocusSoundExperienceButton(){
+        const button = await this.page.locator(focusSoundExperienceLocators.endExperincePoupFocusSoundExperienceButton);
+        await button.click();
+        const expectedPartialText = 'Well';
+      const element = await this.page.locator(focusSoundExperienceLocators.wellDoneExperienceCompletedFocusSoundExperienceButton);
+      const actualText = await element.textContent();
+      await expect(actualText).toContain(expectedPartialText);
+
+    }
+
+    async clickYesHappyFaceFocusSoundExperienceButton(){
+        const button = await this.page.locator(focusSoundExperienceLocators.yesHappyFaceFocusSoundExperienceButton);
+        await button.click();
+    }
+    async clickContinueEndExperienceFocusSoundExperienceButton(){
+        const button = await this.page.locator(focusSoundExperienceLocators.continueEndExperienceFocusSoundExperienceButton);
+        await button.click();
+        const expectedPartialText = 'company';
+        const element = await this.page.locator(focusSoundExperienceLocators.getZensoryForUserOwnCompanyText);
+        const actualText = await element.textContent();
+        await expect(actualText).toContain(expectedPartialText);
+    }
+  async clickFinishAndGoHomeButton(){
+      const button =await this.page.locator(focusSoundExperienceLocators.finishAndGoHomeButton);
+      await button.click();
+      const expectedPartialText = 'Welcome';
+        const element = await this.page.locator(focusSoundExperienceLocators.welcomeToYourZensoryExperienceTextHomePage);
+        const actualText = await element.textContent();
+        await expect(actualText).toContain(expectedPartialText);
+
+
+  }
+
+
+
+    
+        
+    }
+
+
--- tests/pomtest/focusSoundExperince.spec.ts
+++ tests/pomtest/focusSoundExperince.spec.ts
@@ -5,8 +5,8 @@ test("Focus sound experience smokeTest",async({page,baseURL})=>{
    
    const FocusSoundExperienceObject =new FocusSoundExperience(page);
    // await page.goto('${baseURL}');
-   await page.goto("http://localhost:3000/");
-   //await page.goto("https://thezensorywebversion-dev.web.app/");
+   //await page.goto("http://localhost:3000/");
+   await page.goto("https://thezensorywebversion-dev.web.app/");
     await FocusSoundExperienceObject.clickFocusTile();
     await FocusSoundExperienceObject.clickSoundTile();
     await FocusSoundExperienceObject.clickRightArrowSoundExperience();
@@ -16,13 +16,27 @@ test("Focus sound experience smokeTest",async({page,baseURL})=>{
     await FocusSoundExperienceObject.clickLeftArrowSoundExperience();
     await FocusSoundExperienceObject.clickLeftArrowSoundExperience();
     await FocusSoundExperienceObject.clickLeftArrowSoundExperience();
+    await FocusSoundExperienceObject.clickBeachTileSoundExperience();
+    await FocusSoundExperienceObject.clickTenminSoundExperienceOption();
     await FocusSoundExperienceObject.clickFocusSoundExperienceNextButton();
     await FocusSoundExperienceObject.clickLearnWhySoundExperience();
-    await FocusSoundExperienceObject.clickcloseBinauralBeatsSoundExperiencePopupText();
-    await FocusSoundExperienceObject.clickletsBeginFocusSoundExperienceButton();
-    await FocusSoundExperienceObject.clickvolumeFocusSoundExperience();
-    await FocusSoundExperienceObject.binauralFocusSlider();
-
-}
+    await FocusSoundExperienceObject.clickCloseBinauralBeatsSoundExperiencePopupText();
+    await FocusSoundExperienceObject.clickLetsBeginFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickStartExperienceButtonSafariFocusSoundExperience();
+    await FocusSoundExperienceObject.clickVolumeFocusSoundExperience();
+    await FocusSoundExperienceObject.clickBinauralFocusSlider();
+    await FocusSoundExperienceObject.clickMusicSliderFocusSoundExperience();
+    //await FocusSoundExperienceObject.clickNaturescapeSliderFocusSoundExperience();
+    await FocusSoundExperienceObject.clickCloseVolumePopUpFocusSoundEXperience();
+    await FocusSoundExperienceObject.clickplayPauseFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickResumeTextOnTapToResumePopUpFocusSoundExperience();
+    //  await FocusSoundExperienceObject.clickFullScreenFocusSoundExperience();
+    //  await FocusSoundExperienceObject.clickFullScreenFocusSoundExperience();
+    await FocusSoundExperienceObject.clickEndExperienceFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickEndExperincePoupFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickYesHappyFaceFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickContinueEndExperienceFocusSoundExperienceButton();
+    await FocusSoundExperienceObject.clickFinishAndGoHomeButton();
+}  
 
 )
